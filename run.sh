8869361e600322594f3a5e317eaca1ee04a4cf4a
merbi <yuvalmeerbaum@gmail.com>
2023-06-02 14:16:43 +0300

    use doppler to run app

--- run.sh
+++ run.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-CREDS_FILE="/home/$USER/.config/felix-bot/credentials"
-
-if [[ ! -f $CREDS_FILE ]]
-then
-    echo "Could not find credentials file $CREDS_FILE"
-    exit
-fi
-
-source $CREDS_FILE
-
-if [[ -z $TELEGRAM_TOKEN ]]
-then
-    echo "Creds file does not export \$TELEGRAM_TOKEN"
-    exit
-fi
-
 echo "Starting Felix bot!"
-TELEGRAM_TOKEN=$TELEGRAM_TOKEN PLEX_CLAIM=$PLEX_CLAIM docker-compose up -d
+doppler run -- docker-compose up -d
